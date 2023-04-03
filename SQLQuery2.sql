8ca459eb7104e6b1fce1153b5a527adb93f28f25
OmarTarekElHaddad <104107178+OmarTarekElHaddad@users.noreply.github.com>
2023-04-03 21:13:19 +0200

    Add files via upload

---
+++ SQLQuery2.sql
@@ -0,0 +1,12 @@
+DROP Table table_name ;
+DROP Table table_name1;
+DROP table table_name2
+CREATE TABLE table_name (
+  idcli INT, city varchar(50), region varchar(50), country INT);
+
+CREATE TABLE table_name1( product_id INT, name INT, category INT, price_of_revs INT , price_sales INT, supplier INT);
+
+CREATE TABLE table_name2  (idDate int, month int, nameMonth int , quarter int, year INT);
+
+/*CREATE TABLE table_name3  (product id, dateid int, cliid, name , delivery_date int, quantity int, amount float );*/
+
